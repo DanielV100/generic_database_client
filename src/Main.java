84cb32fb79822d3c8e4e66673605bfa120fb8ff7
Daniel <daniel.vollmer@bechtle.com>
2023-03-14 13:30:27 +0100

    optimized project structure

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import view.View;
+
+/*
+Here is just the start of the application - no more or less!
+ */
 public class Main {
     public static void main(String[] args) {
        View view = new View();
--- src/view/View.java
+++ src/view/View.java
@@ -1,14 +1,18 @@
+package view;
+
+import view.UIHelpers;
+
 import javax.swing.*;
 import java.awt.*;
 /*
 This class builds the UI of the application's main frame - here shouldn't be any business logic.
-Therefor Swing and AWT are used. Components such as frames, buttons and labels are created by calling methods from UIHelpers class.
+Therefor Swing and AWT are used. Components such as frames, buttons and labels are created by calling methods from view.UIHelpers class.
  */
 public class View {
     UIHelpers uiHelpers = new UIHelpers();
     private JFrame frameMain = new JFrame();
     private JButton buttonStart = new JButton();
-    View(){
+    public View(){
         //creating the main window and adding a button to it
         UIHelpers.createJFrame(frameMain, "Generic database Client", 1200, 800, null, true)
                 .add(UIHelpers.createJButton(buttonStart, 500, 350, 200, 100, "Hello ON22!", Color.darkGray));
