758d9d3616ccae89e4974897f3054cbd361e8ca0
Daniel <daniel.vollmer@bechtle.com>
2023-04-18 15:47:28 +0200

    implemented function for getting all columns of table

--- src/model/Model.java
+++ src/model/Model.java
@@ -10,7 +10,8 @@ public class Model {
     //create connection to db
     public void connectToDB(String connectionString, String username, String password) throws SQLException {
         dbConnection = DriverManager.getConnection(connectionString, username, password);
-        getAllTablesFromDB();
+        //getAllTablesFromDB();
+        getColumnsFromTable();
     }
     //getting all tables in db and return it as a list
     public List<String> getAllTablesFromDB() throws SQLException {
@@ -21,5 +22,17 @@ public class Model {
             allTables.add(resultSet.getString(1));
         }
         return allTables;
+    }
+    public void getColumnsFromTable() throws SQLException {
+        Statement databaseStatement = dbConnection.createStatement();
+        ResultSet resultSet = databaseStatement.executeQuery(
+                "SELECT COLUMN_NAME\n" +
+                "FROM INFORMATION_SCHEMA.COLUMNS\n" +
+                "WHERE TABLE_NAME = 'dataTable'\n" +
+                "ORDER BY ORDINAL_POSITION"
+        );
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(1));
+        }
     }
 }
