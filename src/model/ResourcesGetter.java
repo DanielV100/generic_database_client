6fba290cba23314d91cb80e822a1e4347b048079
Daniel <daniel.vollmer@bechtle.com>
2023-04-22 14:07:09 +0200

    working

--- src/model/ResourcesGetter.java
+++ src/model/ResourcesGetter.java
@@ -7,7 +7,9 @@ import java.util.Properties;
 
 //Properties of generic_database_client_text.properties will be parsed here
 public class ResourcesGetter {
-    String pathToResourceFile = Thread.currentThread().getContextClassLoader().getResource("").getPath() + "resources/generic_database_client_text.properties";
+   // String pathToResourceFile = Thread.currentThread().getContextClassLoader().getResource("").getPath() + "resources/generic_database_client_text.properties";
+   ClassLoader classLoader = getClass().getClassLoader();
+    String pathToResourceFile = "src/resources/generic_database_client_text.properties";
     Properties appProperties = new Properties();
     public Properties getAppProperties() throws IOException {
         appProperties.load(new FileInputStream(pathToResourceFile));
