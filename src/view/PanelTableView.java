bef05cc032bcc2dfd1c548b27e1628471c384685
Daniel <daniel.vollmer@bechtle.com>
2023-04-25 10:30:20 +0200

    working

--- src/view/PanelTableView.java
+++ src/view/PanelTableView.java
@@ -4,6 +4,7 @@ import controller.DBConnection;
 import resources.Sizes;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
