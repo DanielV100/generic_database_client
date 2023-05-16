e2f26bb937bf5aeaed52a253c284d1e0f5731b29
Daniel <daniel.vollmer@bechtle.com>
2023-05-16 13:52:16 +0200

    added java doc for ressourcegetter

--- src/model/ResourcesGetter.java
+++ src/model/ResourcesGetter.java
@@ -1,17 +1,34 @@
 package model;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-//Properties of generic_database_client_text.properties will be parsed here
+/**
+ * Properties of resources/generic_database_client_text.properties will be parsed here.
+ * @author Daniel
+ */
 public class ResourcesGetter {
     Properties appProperties = new Properties();
+
+    /**
+     * Loading all properties from resources/generic_database_client_text.properties and return it
+     * @return Properties (from resources/generic_database_client_text.properties)
+     * @throws IOException
+     * @author Daniel
+     */
     public Properties getAppProperties() throws IOException {
+        //stream is needed here (otherwise it wouldn't work in the .jar
         appProperties.load(getClass().getClassLoader().getResourceAsStream("resources/generic_database_client_text.properties"));
         return appProperties;
     }
+
+    /**
+     * Getting properties calling method getAppProperties() and get particular property (key is needed)
+     * @param key (of the property)
+     * @return Property value as String
+     * @throws IOException
+     * @author Daniel
+     */
     public String getAppPropertiesWithKey(String key) throws IOException {
         return getAppProperties().getProperty(key);
     }
